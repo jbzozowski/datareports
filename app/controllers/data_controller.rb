18b480f15ed8e6247ae68f28a24621d6b73c09fe
Julius Bzozowski <jbzozowski@gmail.com>
2020-06-28 14:06:07 -0400

    updating resume

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -4,7 +4,7 @@ class DataController < ApplicationController
   end
 
   def download_pdf
-    send_file("#{Rails.root}/app/assets/images/resume_old.pdf", :filename => 'resume_old.pdf', :type => 'application/pdf', :disposition => 'attachment')
+    send_file("#{Rails.root}/app/assets/images/bzozowski.pdf", :filename => 'bzozowski.pdf', :type => 'application/pdf', :disposition => 'attachment')
   end
 
 end
